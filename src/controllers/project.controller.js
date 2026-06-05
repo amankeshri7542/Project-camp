db21c045b1b66275753d07a930da0c54c40768c4
Aman Kumar <aman@Amans-MacBook.local>
2026-06-05 18:06:28 +0530

    Implement createProject, updateProject, deleteProject controllers

--- src/controllers/project.controller.js
+++ src/controllers/project.controller.js
@@ -1,28 +1,74 @@
+import mongoose from "mongoose";
 import { asyncHandler } from "../utils/async-handler.js";
 import { ApiError } from "../utils/api-error.js";
 import { ApiResponse } from "../utils/api-response.js";
 import { User } from "../models/user.models.js";
 import { Project } from "../models/project.models.js";
 import { ProjectMember } from "../models/projectMember.models.js";
+import { UserRolesEnum } from "../utils/constants.js";
 
 const getProjects = asyncHandler(async (req, res) => {
-    // TODO
+    // TODO: aggregation pipeline
 });
 
 const getProjectById = asyncHandler(async (req, res) => {
-    // TODO
+    // TODO: aggregation pipeline
 });
 
 const createProject = asyncHandler(async (req, res) => {
-    // TODO
+    const { name, description } = req.body;
+
+    const project = await Project.create({
+        name,
+        description,
+        createdBy: new mongoose.Types.ObjectId(req.user._id),
+    });
+
+    await ProjectMember.create({
+        user: new mongoose.Types.ObjectId(req.user._id),
+        project: new mongoose.Types.ObjectId(project._id),
+        role: UserRolesEnum.ADMIN,
+    });
+
+    return res
+        .status(201)
+        .json(new ApiResponse(201, project, "Project created successfully"));
 });
 
 const updateProject = asyncHandler(async (req, res) => {
-    // TODO
+    const { name, description } = req.body;
+    const { projectId } = req.params;
+
+    const project = await Project.findByIdAndUpdate(
+        projectId,
+        { name, description },
+        { new: true }
+    );
+
+    if (!project) {
+        throw new ApiError(404, "Project not found");
+    }
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, project, "Project updated successfully"));
 });
 
 const deleteProject = asyncHandler(async (req, res) => {
-    // TODO
+    const { projectId } = req.params;
+
+    const project = await Project.findByIdAndDelete(projectId);
+
+    if (!project) {
+        throw new ApiError(404, "Project not found");
+    }
+
+    // Clean up all project members when project is deleted
+    await ProjectMember.deleteMany({ project: projectId });
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, {}, "Project deleted successfully"));
 });
 
 const getProjectMembers = asyncHandler(async (req, res) => {
